0ba7ab827ef8de8a873f692a4346008fab56a800
CarloCassini <c.cassini87@gmail.com>
2023-07-25 16:26:12 +0200

    finito esercizio 1

--- esercizio 1/mainex1.js
+++ esercizio 1/mainex1.js
@@ -1,1 +1,21 @@
 console.log("wakka");
+let userNumber;
+let output;
+let myStartButton = document.getElementById("input-button");
+
+myStartButton.addEventListener("click", function () {
+  userNumber = parseInt(document.getElementById("user-number-input").value);
+  console.log(userNumber);
+
+  if (isNaN(userNumber)) {
+    output = "inserisci un valore numerico";
+  } else {
+    if (userNumber % 2 == 0) {
+      output = userNumber;
+    } else {
+      output = userNumber + 1;
+    }
+  }
+
+  document.getElementById("valore-output").innerHTML = output;
+});
