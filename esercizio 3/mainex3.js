0982acfbe464433d91c7534d15d3226be699ebb4
CarloCassini <c.cassini87@gmail.com>
2023-07-25 19:37:21 +0200

    add numeri casuali

--- esercizio 3/mainex3.js
+++ esercizio 3/mainex3.js
@@ -1,17 +1,24 @@
-const interiList = [1, 23, 23, 554, 225, 876, 72344, 128, 2349];
+const interiList = [
+  Math.floor(Math.random() * 100),
+  Math.floor(Math.random() * 100),
+  Math.floor(Math.random() * 100),
+  Math.floor(Math.random() * 100),
+  Math.floor(Math.random() * 100),
+];
 
 let output;
 let myStartButton = document.getElementById("input-button");
 
 myStartButton.addEventListener("click", function () {
   output = 0;
-  console.log(interiList);
+  document.getElementById("display-array").innerHTML = " ";
+  document.getElementById("display-array").innerHTML = interiList;
 
   for (let i = 0; i <= interiList.length - 1; i = i + 2) {
     output = output + parseInt(interiList[i]);
-    console.log("output " + output);
   }
 
+  document.getElementById("valore-output").innerHTML = "";
   document.getElementById("valore-output").innerHTML = `
    <div> ${output} </div>`;
 });
