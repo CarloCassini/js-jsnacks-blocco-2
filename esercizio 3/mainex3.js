6b4a4898721d8d84fd9800ac792f19a2b34402df
CarloCassini <c.cassini87@gmail.com>
2023-07-25 19:21:32 +0200

    end ex 3

--- esercizio 3/mainex3.js
+++ esercizio 3/mainex3.js
@@ -1,15 +1,17 @@
-const interiList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const interiList = [1, 23, 23, 554, 225, 876, 72344, 128, 2349];
 
 let output;
 let myStartButton = document.getElementById("input-button");
 
 myStartButton.addEventListener("click", function () {
+  output = 0;
   console.log(interiList);
 
   for (let i = 0; i <= interiList.length - 1; i = i + 2) {
-    console.log(i);
-    console.log("interiList.length" + interiList.length);
-    // document.getElementById("valore-output").innerHTML += `
-    //   <div> ${output} </div>`;
+    output = output + parseInt(interiList[i]);
+    console.log("output " + output);
   }
+
+  document.getElementById("valore-output").innerHTML = `
+   <div> ${output} </div>`;
 });
